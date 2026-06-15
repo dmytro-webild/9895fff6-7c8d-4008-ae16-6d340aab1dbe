558ac5ef57d2328c46556cbb656442f4af6192f4
bender <kudin.dima@gmail.com>
2026-06-15 10:29:21 +0000

    Update src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import HomePage from './pages/HomePage';
 
+import BlogPage from './pages/blog/BlogPage';
+import BlogPostPage from './pages/blog/BlogPostPage';
 import ProjectsPage from "@/pages/ProjectsPage";
 import BarrioDeSalamancaPenthousePage from "@/pages/BarrioDeSalamancaPenthousePage";
 export default function App() {
@@ -11,6 +13,8 @@ export default function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/projects" element={<ProjectsPage />} />
         <Route path="/barrio-de-salamanca-penthouse" element={<BarrioDeSalamancaPenthousePage />} />
+        <Route path="/blog" element={<BlogPage />} />
+        <Route path="/blog/:slug" element={<BlogPostPage />} />
       </Route>
     </Routes>
   );
